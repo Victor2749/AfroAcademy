d008ecc4000b897d489dc33bfd0151f7e726bad5
Victor Carvalho <victcar43@yahoo.com.br>
2022-08-31 18:39:09 -0300

    Estilos header

---
+++ Projetos extras/Projeto extra 2/src/App.jsx
@@ -0,0 +1,13 @@
+import './assets/CSS/global.scss';
+
+import styles from './app.module.scss';
+
+function App() {
+  return (
+    <header className={styles.header}>
+      <p>✓ AfroToDo</p>
+    </header>
+  );
+}
+
+export default App;
